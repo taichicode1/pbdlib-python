8e854ccf94a87ca91b98ae4d97e7d319bb3fbb43
hgirgin <hakan.girgin@idiap.ch>
2020-10-29 15:09:03 +0100

    adding to gui

--- pbdlib/gui/demos.py
+++ pbdlib/gui/demos.py
@@ -2,17 +2,12 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from .interactive import Interactive
+from colorama import init  # need this to print colored on Windows and init() after
 from termcolor import colored
-import sys
-if sys.version_info[0] == 3:
-	import tkinter as tk
-	from tkinter.filedialog import asksaveasfilename
-else:
-	import Tkinter as tk
-	from tkFileDialog import asksaveasfilename
-
+import tkinter as tk
+from tkinter.filedialog import asksaveasfilename
 from matplotlib import gridspec
-from ... import pbdlib as pbd
+
 
 class Robot(object):
 	def __init__(self, T):
@@ -20,15 +15,19 @@ class Robot(object):
 		self.ddx, self.fsx, self.fx = np.array([0., 0.]), np.array([0.]), np.array([0., 0.])
 		self.sensor_mode = 0
 
+
 class InteractiveDemos(Interactive, Robot):
 	"""
 	GUI for recording demonstrations in 2D
 	"""
-	def	__init__(self, filename='test', path='', **kwargs):
-		Interactive.__init__(self)
 
-		self.path = os.path.dirname(pbd.__file__) + '/data/gui/' if path == '' else path
-		
+	def __init__(self, filename='test', path='', plot_function=None, **kwargs):
+		Interactive.__init__(self)
+		Robot.__init__(self, self.simulation_T)
+
+		init()  # need to import colorama to use this, see imports
+
+		self.path = os.path.dirname(__file__) if path == '' else path
 		self.fig = plt.figure(figsize=(15, 8), facecolor='white')
 		self.bindings.update({
 			'q': (self.save_demos, [], "save demos"),
@@ -38,14 +37,19 @@ class InteractiveDemos(Interactive, Robot):
 			'd': (self.clear_demos, [False, True], "clear selected demos"),
 		})
 
-		Robot.__init__(self, self.simulation_T)
-
 		gs = gridspec.GridSpec(1, 2)
 
 		self.filename = filename
 
 		self.ax_x = plt.subplot(gs[0])
 		self.ax_dx = plt.subplot(gs[1])
+
+		if plot_function:
+			if type(plot_function) is list:
+				for fnc in plot_function:
+					fnc(self.ax_x)
+			else:
+				plot_function(self.ax_x)
 
 		self.set_events()
 		self.set_plots()
@@ -74,10 +78,10 @@ class InteractiveDemos(Interactive, Robot):
 		# win.attributes("-topmost", True)
 		# win.attributes("-alpha", 0.4)
 
-
 		try:
 			self.demos = np.load(self.path + filename + '.npy')[()]
-			self.nb_demos = self.demos['x'].__len__(); self.params['current_demo'][2] = self.nb_demos - 1
+			self.nb_demos = self.demos['x'].__len__();
+			self.params['current_demo'][2] = self.nb_demos - 1
 			print(colored('Existing skill, demos loaded', 'green'))
 			self.replot_demos()
 			self.fig.canvas.draw()
@@ -88,7 +92,7 @@ class InteractiveDemos(Interactive, Robot):
 
 	def highlight_demos(self):
 		data = self.demos['x'][self.params['current_demo'][0]]
-		self.plots['current_demo'].set_data(data[:, 0], data[:, 1] )
+		self.plots['current_demo'].set_data(data[:, 0], data[:, 1])
 		self.fig.canvas.draw()
 
 	def plot_sensor_value(self, s, scale=1.):
@@ -99,15 +103,15 @@ class InteractiveDemos(Interactive, Robot):
 
 	def set_plots(self):
 		self.plots.update({
-			'robot_plot':self.ax_x.plot([], [], 'o-', mew=4, mec='orangered', ms=10, mfc='w')[0],
-			'sensor_value':self.ax_x.plot([], [],ls=(0,(1,1)), lw=10)[0],
-			'attractor_plot':self.ax_x.plot([], [], 'o-', mew=4, mec='teal', ms=10, mfc='w')[0],
-			'obj_plot':self.ax_x.plot([], [], 'o-', mew=4, mec='steelblue', ms=10, mfc='w')[0],
+			'robot_plot': self.ax_x.plot([], [], 'o-', mew=4, mec='orangered', ms=10, mfc='w')[0],
+			'sensor_value': self.ax_x.plot([], [], ls=(0, (1, 1)), lw=10)[0],
+			'attractor_plot': self.ax_x.plot([], [], 'o-', mew=4, mec='teal', ms=10, mfc='w')[0],
+			'obj_plot': self.ax_x.plot([], [], 'o-', mew=4, mec='steelblue', ms=10, mfc='w')[0],
 			'current_demo': self.ax_x.plot([], [], lw=3, ls='--', color='orangered')[0],
 			'current_demo_dx': self.ax_dx.plot([], [], lw=3, ls='--', color='orangered')[0]
 		})
 
-		for ax, lim in zip([self.ax_x, self.ax_dx], [100, 25]):
+		for ax, lim in zip([self.ax_x, self.ax_dx], [10, 2.5]):  # [100, 25]
 			ax.set_xlim([-lim, lim])
 			ax.set_ylim([-lim, lim])
 
@@ -115,33 +119,31 @@ class InteractiveDemos(Interactive, Robot):
 		if not self.velocity_mode:
 			m = 1.0
 
-			ddx = ffx/m
+			ddx = ffx / m
 			self.x += self.dt / n_steps * self.dx + 0.5 * self.ddx * (
-																	 self.dt / n_steps) ** 2
+					self.dt / n_steps) ** 2
 			self.dx += self.dt / n_steps * 0.5 * (self.ddx + ddx)
 			self.dxx = np.copy(ddx)
 		else:
-			kp = 0.;
+			kp = 0
 			kv = kp ** 0.5 * 2
 			for i in range(50):
 				ddx = kp * (self.curr_mouse_pos - self.dx)
 				self.dx += self.dt * ddx
 				self.x += self.dt * self.dx + (self.dt ** 2) / 2. * ddx
 
-
 	def timer_event(self, event):
 		if self.is_demonstrating:
 			if self.curr_mouse_pos is None: self.pretty_print('Outside'); return
 
 			# print self.x, self.dx
-			kp = 400.;
+			kp = 400.
 			kv = kp ** 0.5 * 2
 
 			n_steps = 10
 			for i in range(n_steps):
 				ffx = kp * (self.curr_mouse_pos - self.x) - kv * self.dx
 				self.sim_dynamics(ffx)
-
 
 			# self.curr_demo += [np.copy(self.x)]; self.curr_demo_dx += [np.copy(self.dx)]
 
@@ -171,7 +173,7 @@ class InteractiveDemos(Interactive, Robot):
 												curr_demo_arr[:, 1])
 
 			self.plots['current_demo_dx'].set_data(curr_demo_dx_arr[:, 0],
-												curr_demo_dx_arr[:, 1])
+												   curr_demo_dx_arr[:, 1])
 			self.fig.canvas.draw()
 
 	def click_event(self, event):
@@ -185,7 +187,6 @@ class InteractiveDemos(Interactive, Robot):
 
 				self.x = self.demos['x'][-1][0] if self.nb_demos > 0 else np.array([0., 0.])
 				self.dx = self.curr_mouse_pos
-
 
 			[t.start() for t in [self.timer, self.plot_timer]]
 
@@ -201,10 +202,10 @@ class InteractiveDemos(Interactive, Robot):
 		for i in range(self.nb_demos):
 			data = self.demos['x'][i]
 			self.plots['demo_%d' % i] = \
-			self.ax_x.plot(data[:, 0], data[:, 1], lw=2, ls='--')[0]
+				self.ax_x.plot(data[:, 0], data[:, 1], lw=2, ls='--')[0]
 			data = self.demos['dx'][i]
 			self.plots['demo_dx_%d' % i] = \
-			self.ax_dx.plot(data[:, 0], data[:, 1], lw=2, ls='--')[0]
+				self.ax_dx.plot(data[:, 0], data[:, 1], lw=2, ls='--')[0]
 
 	def clear_demos(self, last=False, selected=False):
 		"""
@@ -221,7 +222,8 @@ class InteractiveDemos(Interactive, Robot):
 				self.plots['demo_%d' % (i)].remove()
 				self.plots['demo_dx_%d' % (i)].remove()
 
-			self.nb_demos = len(self.demos['x']); self.params['current_demo'][2] = self.nb_demos - 1
+			self.nb_demos = len(self.demos['x']);
+			self.params['current_demo'][2] = self.nb_demos - 1
 
 			self.replot_demos()
 
@@ -238,8 +240,8 @@ class InteractiveDemos(Interactive, Robot):
 
 			for s in self.demos:
 				self.demos[s] = []
-			self.nb_demos = 0; self.params['current_demo'][2] = self.nb_demos - 1
-
+			self.nb_demos = 0;
+			self.params['current_demo'][2] = self.nb_demos - 1
 
 	def finish_demo(self):
 		"""
@@ -256,25 +258,29 @@ class InteractiveDemos(Interactive, Robot):
 			self.demos[s] += [np.array(self._current_demo[s])]
 			self._current_demo[s] = []
 
-		self.plots['current_demo'].set_data([], []); self.plots['current_demo_dx'].set_data([], [])
-		self.plots['demo_%d' % self.nb_demos] = self.ax_x.plot(curr_demo_arr[:, 0], curr_demo_arr[:, 1], lw=2, ls='--')[0]
-		self.plots['demo_dx_%d' % self.nb_demos] = self.ax_dx.plot(curr_demo_dx_arr[:, 0], curr_demo_dx_arr[:, 1], lw=2, ls='--')[0]
+		self.plots['current_demo'].set_data([], []);
+		self.plots['current_demo_dx'].set_data([], [])
+		self.plots['demo_%d' % self.nb_demos] = self.ax_x.plot(curr_demo_arr[:, 0], curr_demo_arr[:, 1], lw=2, ls='--')[
+			0]
+		self.plots['demo_dx_%d' % self.nb_demos] = \
+		self.ax_dx.plot(curr_demo_dx_arr[:, 0], curr_demo_dx_arr[:, 1], lw=2, ls='--')[0]
 
-		self.nb_demos += 1; self.params['current_demo'][2] = self.nb_demos - 1
+		self.nb_demos += 1;
+		self.params['current_demo'][2] = self.nb_demos - 1
 
 		self.fig.canvas.draw()
-
 
 	def save_demos(self):
 		"""
 		Saving demonstrations with filename prompt
 		:return:
 		"""
-		root = tk.Tk(); root.withdraw()
+		root = tk.Tk();
+		root.withdraw()
 
 		file_path = asksaveasfilename(initialdir=self.path, initialfile=self.filename + '.npy')
 
-		self.pretty_print("Demonstrations saved as\n "+ file_path)
+		self.pretty_print("Demonstrations saved as\n " + file_path)
 
 		np.save(file_path, self.demos)
 
